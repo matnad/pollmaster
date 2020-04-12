b0ab0abf3dd51064066ecfe90a04450e0c07cc34
Matthias Nadler <matnad@gmail.com>
2020-04-12 14:09:38 +0200

    get server count going again

--- cogs/db_api.py
+++ cogs/db_api.py
@@ -13,10 +13,14 @@ class DiscordBotsOrgAPI(commands.Cog):
         self.bot = bot
         self.token = SETTINGS.dbl_token
         self.dblpy = dbl.DBLClient(self.bot, self.token)
+        self.update_stats.start()
 
-    @tasks.loop(minutes=30.0)
+    def cog_unload(self):
+        self.update_stats.cancel()
+
+    @tasks.loop(minutes=10.0)
     async def update_stats(self):
-        """This function runs every 30 minutes to automatically update your server count"""
+        """This function runs every 10 minutes to automatically update your server count"""
         logger.info('Attempting to post server count')
         try:
             await self.dblpy.post_guild_count()
